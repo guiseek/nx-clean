89653a8285640539952bea62d24aca12541a3cd7
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-07-12 19:09:01 -0300

    fix: ngPlural de itens finalizados

---
+++ libs/todo/presentation/src/lib/presenter/todos-default.presenter.spec.ts
@@ -0,0 +1,7 @@
+import { TodosDefault } from './todos-default.presenter';
+
+describe('TodosDefault', () => {
+  it('should create an instance', () => {
+    expect(new TodosDefault()).toBeTruthy();
+  });
+});

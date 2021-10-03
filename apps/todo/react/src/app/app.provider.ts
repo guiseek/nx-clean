ee92ecd00053e752724aae63a0cfcdd955445414
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-10-03 17:35:57 -0300

    chore: prepare to deploy pages

--- apps/todo/react/src/app/app.provider.ts
+++ apps/todo/react/src/app/app.provider.ts
@@ -1,6 +1,6 @@
 import { TodoInMemoryRepository } from '@nx-clean/todo-data-access';
 import { TodoRepository } from '@nx-clean/todo-domain';
-import { Injector } from './injector';
+import { Injector } from '@nx-clean/core';
 import {
   TodoPresenter,
   TodoDefaultPresenter,
@@ -11,7 +11,7 @@ export const injector = Injector.create([
     provide: TodoRepository,
     useFactory: () => {
       return new TodoInMemoryRepository([
-        { id: '1', title: 'test', completed: false },
+        { id: '1', title: 'Opa', completed: true },
       ]);
     },
   },

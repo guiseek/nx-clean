035323d2e0c09601ed5019f5d4923014828659e3
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-10-06 17:50:33 -0300

    feat(todo-angular): add angular material

---
+++ apps/todo/angular/src/app/shared/shared.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { A11yModule } from '@angular/cdk/a11y';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+import { CheckboxComponent } from './checkbox/checkbox.component';
+
+
+
+@NgModule({
+  declarations: [
+    CheckboxComponent
+  ],
+  imports: [
+    CommonModule,
+    A11yModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CheckboxComponent,
+    A11yModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule
+  ]
+})
+export class SharedModule { }

74ac87f31b62a9529fd7863eb3f3299af1f764ce
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-10-09 21:25:41 -0300

    feat(core): injection token type

--- libs/core/src/lib/di/core/injection-token.ts
+++ libs/core/src/lib/di/core/injection-token.ts
@@ -1,3 +1,14 @@
-export class InjectionToken {
-  constructor(public injectionIdentifier: string) {}
+export class InjectionToken<T = any> {
+  constructor(
+    protected injectionIdentifier: string,
+    options?: {
+      factory: () => T;
+    }
+  ) {
+    console.log(options);
+  }
+
+  toString(): string {
+    return this.injectionIdentifier
+  }
 }

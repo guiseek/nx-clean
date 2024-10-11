9d4b04084fc9d348d1c121b029c61890e8254e0b
Estevan <estevanbartmann@gmail.com>
2024-10-11 16:10:39 -0300

    chore(update): migrate latest version and fix tests (#69)

--- libs/plugin/core/src/utils/get-project-import-path.ts
+++ libs/plugin/core/src/utils/get-project-import-path.ts
@@ -1,8 +1,9 @@
 import { ProjectConfiguration, readJsonFile } from '@nx/devkit';
 import { TSConfigBase } from '../interfaces';
+import { getRootTsConfigPath } from '@nx/js';
 
 export function getProjectImportPath({ sourceRoot }: ProjectConfiguration) {
-  const { compilerOptions } = readJsonFile<TSConfigBase>('tsconfig.base.json');
+  const { compilerOptions } = readJsonFile<TSConfigBase>(getRootTsConfigPath());
 
   return Object.keys(compilerOptions.paths).find((importPath) => {
     return compilerOptions.paths[importPath].find(
